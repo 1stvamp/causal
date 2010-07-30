c580f0e710845ce0ed45dea0cf544ec288c7bf53
Wesley Mason <wes [at] 1stvamp.org>
2010-07-30 10:34:28 +0100

    Prototype twitter service

--- helios/twitter/service.py
+++ helios/twitter/service.py
@@ -1,2 +1,32 @@
-name = 'twitter'
-display = 'Twitter'
+__version__ = '0.1.1'
+
+from django.shortcuts import render_to_response, redirect
+from helios.main.models import Service, ServiceItem
+from helios.twitter.utils import get_api, user_login
+
+display_name = 'Twitter'
+
+def get_items(user, model_instance=None):
+    items = []
+    serv = model_instance or get_model_instance(user)
+
+    api = get_api(serv)
+    if not api:
+        r = user_login(serv.oauth)
+        if r:
+            return r
+        else:
+            return items
+
+    timeline = api.user_timeline(count=50)
+    for status in timeline:
+        item = ServiceItem()
+        item.body = status.text
+        item.created =status.created_at
+        if status.geo:
+            item.location['lat'] = status.geo[0]
+            item.location['long'] = status.geo[1]
+        item.service = serv
+
+def get_model_instance(user):
+    return Service.objects.get(user=user, app_name=__package__)
--- helios/twitter/views.py
+++ helios/twitter/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import redirect
+
+def verify_auth(request):
+    request.session['helios_twitter_oauth_verify_token'] = request.GET.get('oauth_verifier')
+    return_url = request.session.get('helios_twitter_oauth_return_url', None) or 'history'
+    return redirect(return_url)
