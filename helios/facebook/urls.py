fb80ac82ad2c35f95cabf211045a9abdede6f6aa
Wesley Mason <wes [at] 1stvamp.org>
2010-08-07 00:20:57 +0800

    Base facebook service (non-working)

---
+++ helios/facebook/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+base_path = r'^facebook/'
+shortcut = 'facebook-urls'
+
+urlpatterns = patterns('',
+#    url(r'^$', 'helios.facebook.views.verify_auth', name='helios-facebook-callback'),
+#    url(r'^auth$', 'helios.facebook.views.auth', name='helios-facebook-auth'),
+)
