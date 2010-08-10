252961764afc63a2d5c825032d44a6a39f5556b0
Wesley Mason <wes@1stvamp.org>
2010-08-10 22:41:38 +0800

    Moved to ajax callback for services and sorting in JS

--- helios/main/views.py
+++ helios/main/views.py
@@ -1,24 +1,28 @@
+from time import mktime
 from datetime import datetime, timedelta, date
 
+from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.template import RequestContext
+from django.utils import simplejson
+from django.utils.html import urlize
 
 from helios.main.forms import RegistrationForm
 from helios.main.models import *
 
-@login_required(redirect_field_name='redirect_to')
 def history(request, user_id=None):
     template_values = {}
+
     if user_id:
         user = get_object_or_404(User, pk=user_id)
     else:
         if request.user.is_authenticated() and request.user.is_active:
             user = request.user
         else:
-            return redirect('register')
+            return redirect('login')
 
     services = UserService.objects.filter(user=request.user)
     template_values['services'] = services
@@ -35,17 +39,6 @@ def history(request, user_id=None):
         day_names[i] = last.strftime('%A')
         days_to_i[day_names[i]] = i
 
-    for service in services:
-        items = service.app.module.get_items(request.user, day_one, service)
-        if items:
-            for item in items:
-                if item.created.date() > day_one:
-                    days[days_to_i[item.created.strftime('%A')]].append(item)
-
-    if days:
-        for day in days:
-            day.sort(key=lambda item:item.created, reverse=True)
-
     template_values['days'] = days
     template_values['day_names'] = day_names
 
@@ -54,6 +47,36 @@ def history(request, user_id=None):
         template_values,
         context_instance=RequestContext(request)
     )
+
+def history_callback(request, service_id):
+    template_values = {}
+    service = get_object_or_404(UserService, pk=service_id)
+
+    days = []
+    days_to_i = {}
+    day_one = date.today() - timedelta(days=7)
+    today = date.today()
+
+    for i in range(0,7):
+        last = today - timedelta(days=i)
+        days.append([])
+        days_to_i[last.strftime('%A')] = i
+
+    items = service.app.module.get_items(request.user, day_one, service)
+    if items:
+        for item in items:
+            if item.created.date() > day_one:
+                item_dict = {
+                    'title': item.title,
+                    'body': urlize(item.body),
+                    'created': mktime(item.created.timetuple()),
+                    'location': item.location,
+                    'class_name' : item.class_name,
+                    'has_location': item.has_location(),
+                }
+                days[days_to_i[item.created.strftime('%A')]].append(item_dict)
+
+    return HttpResponse(simplejson.dumps(days))
 
 def register(request):
     form = RegistrationForm()
