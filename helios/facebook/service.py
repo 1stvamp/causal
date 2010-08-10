9a3214324d7bcce4dced0738e03f28c2a18dc753
Wesley Mason <wes@1stvamp.org>
2010-08-10 12:48:17 +0100

    Working FB app

--- helios/facebook/service.py
+++ helios/facebook/service.py
@@ -1,6 +1,6 @@
 import httplib2
 import oauth2 as oauth
-from datetime import datetime, timedelta
+from datetime import datetime
 from django.utils.safestring import mark_safe
 from helios.main.models import ServiceItem, AccessToken
 from helios.main.service_utils import get_model_instance
@@ -8,31 +8,32 @@ from facegraph.fql import FQL
 
 display_name = 'Facebook'
 
-SELECT_FQL = """SELECT post_id, actor_id, target_id, message
+SELECT_FQL = """SELECT post_id, actor_id, target_id, updated_time, message
 FROM stream
-WHERE filter_key
-IN (
+WHERE filter_key IN (
     SELECT filter_key
     FROM stream_filter
     WHERE uid = me() AND type = 'newsfeed'
-)"""
+)
+AND (actor_id = me() OR target_id =  me() OR source_id = me())"""
 
 def get_items(user, since, model_instance=None):
     serv = model_instance or get_model_instance(user, __package__)
     items = []
 
     try:
-        at = AccessToken.object.get(service=serv)
+        at = AccessToken.objects.get(service=serv)
 
         q = FQL(at.oauth_token)
         results = q(SELECT_FQL)
 
         for result in results:
             item = ServiceItem()
+            item.created = datetime.fromtimestamp(result.updated_time)
             item.body = result.message
             item.service = serv
             items.append(item)
-    except:
-        pass
+    except Exception, e:
+        print e
 
     return items
