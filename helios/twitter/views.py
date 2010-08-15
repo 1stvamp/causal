2214adc9f0ed200d31349c004728fd86927ee500
Chris Hannam <bassdread@gmail.com>
2010-08-16 00:16:45 +0100

    Added callback handling from twitter Had to add oauth_verify to request token to make it work

--- helios/twitter/views.py
+++ helios/twitter/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from helios.main.models import UserService, RequestToken, OAuthSetting, ServiceApp
 from helios.twitter.utils import user_login
-from helios.main.service_utils import get_model_instance
+from helios.main.service_utils import get_model_instance, generate_access_token
 
 @login_required(redirect_field_name='redirect_to')
 def verify_auth(request):
@@ -10,6 +10,7 @@ def verify_auth(request):
     request_token = RequestToken.objects.get(service=service)
     request_token.oauth_verify = request.GET.get('oauth_verifier')
     request_token.save()
+    generate_access_token(service, request_token)
     return_url = request.session.get('helios_twitter_oauth_return_url', None) or 'history'
     return redirect(return_url)
 
