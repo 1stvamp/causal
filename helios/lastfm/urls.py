b010b0709c39241fdac014f87a136123439c5250
Chris Hannam <bassdread@gmail.com>
2010-08-20 23:58:50 +0100

    Added last.fm form for username

--- helios/lastfm/urls.py
+++ helios/lastfm/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 base_path = r'^lastfm/'
 shortcut = 'lastfm-urls'
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    url(r'^auth$', 'helios.lastfm.views.auth', name='helios-lastfm-auth'),
 )
