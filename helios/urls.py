8bebca9c447437c0d832cdf1445b57741783bc64
Wesley Mason <wes@1stvamp.org>
2010-09-29 15:46:48 +0100

    Abstracted admin url patterns enabling to settings, no need to edit urls.py source now

--- helios/urls.py
+++ helios/urls.py
@@ -20,12 +20,6 @@ admin.autodiscover()
 cache_time = getattr(settings, 'ITEM_CACHE_TIME', 60 * 30)
 
 urlpatterns = patterns('',
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    url(settings.ADMIN_URL, include(admin.site.urls), name='admin'),
-
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^accounts/logout/$', logout_view, name='logout'),
     url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', name='password-reset'),
@@ -40,6 +34,15 @@ urlpatterns = patterns('',
     url(r'^history/ajax/(?P<service_id>\d+)$', cache_page(history_callback, cache_time), name='history-callback'),
     url(r'^history/ajax/(?P<service_id>\d+)$', history_callback, name='history-callback'),
 )
+
+if getattr(settings, 'ENABLE_ADMIN', False):
+    urlpatterns += patterns('',
+        url(settings.ADMIN_URL, include(admin.site.urls), name='admin'),
+    )
+if getattr(settings, 'ENABLE_ADMIN_DOCS', False):
+    urlpatterns += patterns('',
+        url(r'%sdoc/' % (settings.ADMIN_URL,), include('django.contrib.admindocs.urls')),
+    )
 
 for service_name in settings.INSTALLED_SERVICES:
     service_urls = import_module("%s.urls" % (service_name,))
