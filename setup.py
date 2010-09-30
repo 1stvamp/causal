b680fd90ddf6853a717202f4358d47a6cfa8d4ef
Wesley Mason <wes@1stvamp.org>
2010-09-30 12:46:33 +0100

    Updated helios to causal on setup handler

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ except ImportError:
     from setuptools import setup, find_packages
 
 setup(
-        name='helios',
+        name='causal',
         description='Open source lifestream aggregator'
         version='0.2',
         author='Chris Hannam',
         author_email='bassdread@gmail.com',
-        url='http://github.com/bassdread/helios',
+        url='http://github.com/bassdread/causal',
         ackages=find_packages(exclude=['ez_setup']),
         setup_requires=open('virtualenv_build/base_requirements.txt', 'r').readlines(),
         extras_require={
             'services': open('virtualenv_build/extras_requirements.txt', 'r').readlines(),
         }
-        #license='Apache License 2.0'
+        license='Apache License 2.0'
 )
