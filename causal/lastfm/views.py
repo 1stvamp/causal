3cc6939145009fc8ac5edb23146d7b583713deb6
Chris Hannam <ch@chrishannam.co.uk>
2010-10-03 22:13:28 +0100

    Fixed last to use an api key stored as oauth setting

--- causal/lastfm/views.py
+++ causal/lastfm/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from causal.main.models import UserService, RequestToken, OAuthSetting, ServiceApp, AccessToken
 from causal.main.service_utils import get_model_instance, user_login, generate_access_token, get_module_name
+from datetime import date, timedelta
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 # Yay, let's recreate __package__ for Python <2.6
 MODULE_NAME = get_module_name(__name__)
@@ -16,10 +19,6 @@ def auth(request):
         username = request.POST['username']
 
         # Now we have a userservice and app create a request token
-        request_token = RequestToken(service=service)
-        request_token.created = datetime.now()
-        request_token.save()
-
         access_token = AccessToken(service=service)
         access_token.username = username
         access_token.created = datetime.now()
@@ -30,3 +29,18 @@ def auth(request):
         service.save()
 
     return redirect(reverse('profile'))
+
+@login_required(redirect_field_name='redirect_to')
+def stats(request):
+    """Display stats based on checkins."""
+    service = get_model_instance(request.user, MODULE_NAME)
+    template_values = {}
+    
+    # get users recently played tracks
+    template_values['recent_tracks'] = get_items(request.user, date.today() - timedelta(days=7), service)
+    
+    return render_to_response(
+      service.app.module_name + '/stats.html',
+      template_values,
+      context_instance=RequestContext(request)
+    )
