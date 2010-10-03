7e64347a3aa2430accea5866a51ec8aa9f588cf4
Chris Hannam <ch@chrishannam.co.uk>
2010-10-03 22:21:54 +0100

    Enabled stats url

--- causal/lastfm/urls.py
+++ causal/lastfm/urls.py
@@ -5,4 +5,5 @@ shortcut = 'lastfm-urls'
 
 urlpatterns = patterns('',
     url(r'^auth$', 'causal.lastfm.views.auth', name='causal-lastfm-auth'),
+    url(r'^stats$', 'causal.lastfm.views.stats', name='causal-lastfm-stats'),
 )
