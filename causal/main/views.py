17d40a633fa6d157e95b2309142dbdfa137b90c3
Wesley Mason <wes@1stvamp.org>
2010-10-05 01:05:02 +0800

    Removed unnecessary import of User model

--- causal/main/views.py
+++ causal/main/views.py
@@ -6,7 +6,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404, redirect
-from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.utils import simplejson
 from django.utils.html import urlize
