b54a9d2e020f453983892a08a109e692d194a26d
Wesley Mason <wes@1stvamp.org>
2010-10-11 22:20:01 +0100

    Fixed ENABLE_REGISTRATION=False killing everything

--- causal/urls.py
+++ causal/urls.py
@@ -16,30 +16,45 @@ if settings.DEBUG:
 from django.contrib import admin
 admin.autodiscover()
 
+# These custom error handlers return the correct Http codes for their respective
+# errors, as opposed to a 200 as normally returned.
+# We can also redirect to templates wherever we like here.
+handler404 = '%s.return_404' % (settings.ROOT_URLCONF,)
+handler500 = '%s.return_500' % (settings.ROOT_URLCONF,)
+
+def return_404(request):
+    return HttpResponseNotFound(render_to_string("404.html"))
+
+def return_500(request):
+    return HttpResponseServerError(render_to_string("500.html"))
 
 cache_time = getattr(settings, 'ITEM_CACHE_TIME', 60 * 30)
 
-urlpatterns = patterns('',
+urlpatterns = patterns('')
+
+if not getattr(settings, 'ENABLE_REGISTRATION', False):
+    # Override registration URL if disabled
+    urlpatterns += patterns('',
+        url(r'^accounts/register/$', return_404, name='registration_register')
+    )
+
+urlpatterns += patterns('',
     url(r'^accounts/settings/$', user_settings, name='user-settings'),
     url(r'^accounts/settings/enable-service/(?P<app_id>\d+)$', enable_service, name='enable-service'),
     url(r'^accounts/settings/sharing/$', sharing_prefs, name='share-prefs'),
+    url(
+        r'^accounts/logout/$',
+        'django.contrib.auth.views.logout',
+        {'next_page': getattr(settings, 'LOGOUT_REDIRECT_URL', '/'),'template_name': 'registration/logout.html'},
+        name='auth_logout'
+    ),
+    url(r'^accounts/', include('registration.urls')),
 
     url(r'^$', index, name='home'),
     url(r'^history/$', history, name='history'),
     url(r'^history/(?P<user_id>\d+)$', history, name='user-history'),
     url(r'^history/ajax/(?P<service_id>\d+)$', cache_page(history_callback, cache_time), name='history-callback'),
 )
-
-if getattr(settings, 'ENABLE_REGISTRATION', False):
-    next_page = getattr(settings, 'LOGOUT_REDIRECT_URL', '/')
-    urlpatterns += patterns('',
-        url(
-            r'^accounts/logout/$',
-            'django.contrib.auth.views.logout',
-            {'next_page': next_page,'template_name': 'registration/logout.html'}
-        ),
-        url(r'^accounts/', include('registration.urls')),
-    )
 
 if getattr(settings, 'ENABLE_ADMIN', False):
     urlpatterns += patterns('',
@@ -63,16 +78,4 @@ if settings.SERVE_STATIC:
             'document_root': settings.MEDIA_ROOT }
          ),
     )
-
-# These custom error handlers return the correct Http codes for their respective
-# errors, as opposed to a 200 as normally returned.
-# We can also redirect to templates wherever we like here.
-handler404 = '%s.return_404' % (settings.ROOT_URLCONF,)
-handler500 = '%s.return_500' % (settings.ROOT_URLCONF,)
-
-def return_404(request):
-    return HttpResponseNotFound(render_to_string("404.html"))
-
-def return_500(request):
-    return HttpResponseServerError(render_to_string("500.html"))
 
