55bd17e183b1964a2cc2ede75e75bc2a7760ec14
Chris Hannam <ch@chrishannam.co.uk>
2010-10-19 13:28:58 +0100

    Added enable method

--- causal/facebook/service.py
+++ causal/facebook/service.py
@@ -19,6 +19,10 @@ WHERE filter_key IN (
 )
 AND (actor_id = me() OR target_id =  me() OR source_id = me())"""
 
+def enable():
+    """Setup and authorise the service."""
+    return redirect('causal-facebook-auth')
+
 def get_items(user, since, model_instance=None):
     serv = model_instance or get_model_instance(user, __name__)
     items = []
