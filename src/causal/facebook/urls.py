7841fad7149f8e7a6bd31c4e88a9f4442b7ac769
Chris Hannam <ch@chrishannam.co.uk>
2010-10-24 21:50:46 +0800

    Added outline for facebook stats

--- src/causal/facebook/urls.py
+++ src/causal/facebook/urls.py
@@ -6,4 +6,5 @@ shortcut = 'facebook-urls'
 urlpatterns = patterns('',
     url(r'^$', 'causal.facebook.views.verify_auth', name='causal-facebook-callback'),
     url(r'^auth$', 'causal.facebook.views.auth', name='causal-facebook-auth'),
+    url(r'^stats/(?P<service_id>\d+)$', 'causal.facebook.views.stats', name='causal-facebook-stats'),
 )
