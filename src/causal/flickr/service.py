a67f530f06b514f99d4c255038ed1a26f03c184e
Wesley Mason <wes@1stvamp.org>
2010-10-24 13:29:32 +0100

    Removed _as_json functions from services, closes #13

--- src/causal/flickr/service.py
+++ src/causal/flickr/service.py
@@ -24,7 +24,7 @@ def get_items(user, since, model_instance):
         )
     except Exception, e:
         raise LoggedServiceError(original_exception=e)
-    
+
     photos_json = photos_json.replace('jsonFlickrApi(', '')
     photos_json = photos_json.rstrip(')')
     photos = simplejson.loads(photos_json)
@@ -38,7 +38,7 @@ def get_items(user, since, model_instance):
         epoch = p['photo']['dateuploaded']
 
         item = ServiceItem()
-        item.location = {}        
+        item.location = {}
         item.title = p['photo']['title']['_content']
         item.body = p['photo']['urls']['url'][0]['_content']
         item.created = datetime.fromtimestamp(float(epoch))
@@ -53,47 +53,5 @@ def get_items(user, since, model_instance):
             item.location['lat'] = p['photo']['location']['latitude']
             item.location['long'] = p['photo']['location']['longitude']
         item.user = user
-        items.append(item)
-    return items
-
-def get_items_as_json(user, since, model_instance=None):
-    serv = model_instance or get_model_instance(user, __name__)
-    items = []
-
-    at = AccessToken.objects.get(service=serv)
-    flickr = flickrapi.FlickrAPI(at.api_token)
-
-    photos_json = flickr.photos_search(
-        user_id=at.username,
-        per_page='10',
-        format='json'
-    )
-    photos_json = photos_json.replace('jsonFlickrApi(', '')
-    photos_json = photos_json.rstrip(')')
-    photos = simplejson.loads(photos_json)
-
-    for photo in photos['photos']['photo']:
-        # flickr.photos.getInfo
-        pic = flickr.photos_getInfo(photo_id=photo['id'], format='json')
-        pic = pic.replace('jsonFlickrApi(', '')
-        pic = pic.rstrip(')')
-        p = simplejson.loads(pic)
-        epoch = p['photo']['dateuploaded']
-
-        item = ServiceItem()
-        item.location = {}        
-        item.title = p['photo']['title']['_content']
-        item.body = p['photo']['urls']['url'][0]['_content']
-        item.created = datetime.fromtimestamp(float(epoch))
-        item.service = serv
-        item.link_back = p['photo']['urls']['url'][0]['_content']
-        item.tags = p['photo']['tags']['tag']
-        item.favorite = p['photo']['isfavorite']
-        item.number_of_comments = p['photo']['comments']['_content']
-        item.url = "http://farm%s.static.flickr.com/%s/%s_%s_m_d.jpg" %(p['photo']['farm'], p['photo']['server'], p['photo']['id'], p['photo']['secret'])
-        # add location
-        if p['photo'].has_key('location'):
-            item.location['lat'] = p['photo']['location']['latitude']
-            item.location['long'] = p['photo']['location']['longitude']
         items.append(item)
     return items
--- src/causal/foursquare/service.py
+++ src/causal/foursquare/service.py
@@ -44,34 +44,3 @@ def get_items(user, since, model_instance=None):
             items.append(item)
             del(item)
     return items
-
-def get_items_as_json(user, since, model_instance=None):
-    serv = model_instance or get_model_instance(user, __name__)
-    items = []
-
-    checkins = get_data(serv, 'http://api.foursquare.com/v1/history.json')
-    if checkins and checkins.has_key('checkins'):
-        for checkin in checkins['checkins']:
-            item = ServiceItem()
-            item.location = {}
-            item.link_back = 'http://foursquare.com/venue/%s' % checkin['venue']['id']
-            item.title = checkin['venue']['name']
-            if checkin.has_key('shout') and checkin['shout']:
-                item.body = checkin['shout']
-            else:
-                item.body = checkin['venue']['city']
-            if checkin['venue'].has_key('geolat') and checkin['venue']['geolat']:
-                item.location['lat'] = checkin['venue']['geolat']
-                item.location['long'] = checkin['venue']['geolong']
-            item.created = datetime.strptime(
-                checkin['created'].replace(' +0000', ''),
-                '%a, %d %b %y %H:%M:%S') + timedelta(hours=datetime.now().utcoffset() or 0)
-            item.service = serv
-            if checkin['venue'].has_key('primarycategory'):
-                item.icon = checkin['venue']['primarycategory']['iconurl']
-            items.append(item)
-            del(item)
-    else:
-        # deal with errors
-        pass
-    return items
