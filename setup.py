87d621c133eaef5d66cc16a8b1ddee3e7442eee5
Wesley Mason <wes@1stvamp.org>
2010-10-29 13:41:50 +0100

    Fixed conflict in setup.py

--- setup.py
+++ setup.py
@@ -9,19 +9,13 @@ setup(
         name='causal',
         description='Open source lifestream aggregator',
         version='0.9',
-<<<<<<< HEAD
-        author='Chris Hannam',
-        author_email='bassdread@gmail.com',
-        url='http://github.com/bassdread/causal',
-=======
         author='Project Causal Team',
         author_email='team@projectcausal.com',
         url='http://github.com/causality/causal',
->>>>>>> upstream/master
         packages=find_packages('src', exclude=['ez_setup']),
         package_dir={'': 'src'},
         setup_requires=open('virtualenv_build/base_requirements.txt', 'r').readlines(),
-        install_requires = ['setuptools'],
+        install_requires=['setuptools'],
         extras_require={
             'services': open('virtualenv_build/extras_requirements.txt', 'r').readlines(),
         },
