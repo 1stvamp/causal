2a0c27d7070efe1bf3b53f73ed1333e8c52d403a
Wesley Mason <wes@1stvamp.org>
2010-12-07 08:17:46 +0000

    Fixed setup.py for compatibility in PyPM builds, closes #47

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 try:
     from setuptools import setup, find_packages
 except ImportError:
@@ -5,16 +6,18 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
+INSTALLER_DIR = os.path.normpath(os.path.dirname(__file__))
+
 setup(
         name='causal',
         description='Open source lifestream aggregator',
-        version='0.9',
+        version='0.9.1',
         author='Project Causal Team',
         author_email='team@projectcausal.com',
         url='http://github.com/causality/causal',
         packages=find_packages('src', exclude=['ez_setup']),
         package_dir={'': 'src'},
-        setup_requires=open('virtualenv_build/base_requirements.txt', 'r').readlines(),
+        setup_requires=open(os.path.join(INSTALLER_DIR, 'virtualenv_build/base_requirements.txt'), 'r').readlines(),
         install_requires=['setuptools'],
         extras_require={
             'services': open('virtualenv_build/extras_requirements.txt', 'r').readlines(),
