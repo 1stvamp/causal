52d010cb36e0957d2df29bc4c892178d9dba3bca
Chris Hannam <ch@chrishannam.co.uk>
2010-12-17 16:12:55 +0000

    Added test for correct date on tweet closes #53

--- src/causal/twitter/service.py
+++ src/causal/twitter/service.py
@@ -28,7 +28,7 @@ def get_items(user, since, model_instance=None):
         return False
     
     try:
-        # fetch 200 tweets, she be enough for a week...
+        # fetch 200 tweets, should be enough for a week...
         timeline = api.user_timeline(count=200, include_rts='true')
         
     except TweepError, e:
@@ -37,24 +37,27 @@ def get_items(user, since, model_instance=None):
         screen_name = api.me().screen_name
         
         for status in timeline:
-            item = ServiceItem()
-            item.location = {}
-            tt = TwitterText(status.text)
-            tt.autolink.auto_link_usernames_or_lists()
-            tt.autolink.auto_link_hashtags()
-            item.body = unicode(tt.text)
-
-            if re.search("http://yfrog.com/\S*", item.body) \
-               or re.search("http://twitpic.com/\S*", item.body):
-                item.pic_link = True
             
-            item.created = status.created_at
-            item.link_back = "http://twitter.com/%s/status/%s" % (screen_name, str(status.id))
-            if status.geo:
-                item.location['lat'] = status.geo['coordinates'][0]
-                item.location['long'] = status.geo['coordinates'][1]
-            item.service = serv
-            item.user = user
-            items.append(item)
+            # we are interested in tweets since
+            if status.created_at.date() > since - timedelta(days=1):
+                item = ServiceItem()
+                item.location = {}
+                tt = TwitterText(status.text)
+                tt.autolink.auto_link_usernames_or_lists()
+                tt.autolink.auto_link_hashtags()
+                item.body = unicode(tt.text)
+    
+                if re.search("http://yfrog.com/\S*", item.body) \
+                   or re.search("http://twitpic.com/\S*", item.body):
+                    item.pic_link = True
+                
+                item.created = status.created_at
+                item.link_back = "http://twitter.com/%s/status/%s" % (screen_name, str(status.id))
+                if status.geo:
+                    item.location['lat'] = status.geo['coordinates'][0]
+                    item.location['long'] = status.geo['coordinates'][1]
+                item.service = serv
+                item.user = user
+                items.append(item)
         
     return items
