960718740efe79b25056cfb41360a1dfefca0820
Wesley Mason <wes@1stvamp.org>
2011-01-06 16:10:36 +0000

    Fixed multiple initial fixtures being loaded

--- src/causal/facebook/migrations/0001_load_auth_fixture.py
+++ src/causal/facebook/migrations/0001_load_auth_fixture.py
@@ -7,7 +7,7 @@ from django.db import models
 class Migration(DataMigration):
     def forwards(self, orm):
         from django.core.management import call_command
-        call_command("loaddata", "auth_data.json")
+        call_command("loaddata", "facebook/fixtures/auth_data.json")
 
     def backwards(self, orm):
         "Write your backwards methods here."
