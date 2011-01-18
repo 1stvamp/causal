00146a7031bf8e45ee33722938977b6c19b78e2f
Chris Hannam <ch@chrishannam.co.uk>
2011-01-18 07:34:15 +0000

    Minor refactor and fixed to use global redirect

--- src/causal/github/views.py
+++ src/causal/github/views.py
@@ -1,15 +1,18 @@
+""" Handler for URLs for the http://github.com service.
+GitHub doesn't really have a decent oauth service so again we 
+are hitting public rss feeds and processing those.
+"""
+
 from datetime import datetime
-from django.core.urlresolvers import reverse
-from django.shortcuts import redirect
-from django.contrib.auth.decorators import login_required
-from causal.main.models import UserService, RequestToken, OAuthSetting, ServiceApp, AccessToken
-from causal.main.service_utils import get_model_instance, user_login, generate_access_token, get_module_name
-from django.template import RequestContext
-from django.shortcuts import render_to_response, get_object_or_404
-from causal.main.decorators import can_view_service
 from causal.github.service import get_items
+from causal.main.decorators import can_view_service
+from causal.main.models import UserService, AccessToken
+from causal.main.service_utils import get_model_instance,  \
+     get_module_name, settings_redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.template import RequestContext
 from datetime import date, timedelta
-from BeautifulSoup import BeautifulSoup, SoupStrainer
 
 # Yay, let's recreate __package__ for Python <2.6
 MODULE_NAME = get_module_name(__name__)
@@ -17,12 +20,14 @@ MODULE_NAME = get_module_name(__name__)
 @login_required(redirect_field_name='redirect_to')
 def auth(request):
     """We dont need a full oauth setup just a username."""
+
     service = get_model_instance(request.user, MODULE_NAME)
     if service and request.method == 'POST':
         username = request.POST['username']
 
         # Delete existing token
         AccessToken.objects.filter(service=service).delete()
+        
         # Before creating a new one
         AccessToken.objects.create(
             service=service,
@@ -34,14 +39,16 @@ def auth(request):
         service.setup = True
         service.public = True
         service.save()
-    return_url = request.session.get('causal_foursquare_oauth_return_url', None) or '/' + request.user.username
-    return redirect(return_url)
+    
+    return redirect(settings_redirect(request))
 
 @can_view_service
 def stats(request, service_id):
     """Create up some stats."""
+
     service = get_object_or_404(UserService, pk=service_id)
     commits = get_items(request.user, date.today() - timedelta(days=7), service)
+
     return render_to_response(
         service.template_name + '/stats.html',
         {'commits': commits},
