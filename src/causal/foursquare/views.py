789670e96f8304e9cb161b9200cced569eec63e8
Chris Hannam <ch@chrishannam.co.uk>
2011-01-18 07:42:04 +0000

    Refectored a bit and added global settings redirect

--- src/causal/foursquare/views.py
+++ src/causal/foursquare/views.py
@@ -1,33 +1,43 @@
-from django.shortcuts import redirect
-from django.contrib.auth.decorators import login_required
-from causal.main.models import UserService, RequestToken, OAuthSetting, ServiceApp
-from causal.main.service_utils import get_model_instance, user_login, generate_access_token, get_module_name
+"""Handle URLs for the http://foursquare.com service. We are using
+full blown oauth to access the user's details.
+"""
+
 from causal.foursquare.service import get_items
-from datetime import date, timedelta
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext
 from causal.main.decorators import can_view_service
+from causal.main.models import UserService, RequestToken, ServiceApp
+from causal.main.service_utils import get_model_instance, user_login, \
+     generate_access_token, get_module_name, settings_redirect
+from datetime import date, timedelta
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import redirect
+from django.template import RequestContext
 
 # Yay, let's recreate __package__ for Python <2.6
 MODULE_NAME = get_module_name(__name__)
 
 @login_required(redirect_field_name='redirect_to')
 def verify_auth(request):
+    """Handle the redirect from foursquare as part of the oauth process."""
+    
     service = get_model_instance(request.user, MODULE_NAME)
     request_token = RequestToken.objects.get(service=service)
     request_token.oauth_verify = request.GET.get('oauth_verifier')
     request_token.save()
-    return_url = request.session.get('causal_foursquare_oauth_return_url', None) or '/' + request.user.username
+    
     generate_access_token(service, request_token)
     service.setup = True
     service.public = True
     service.save()
     request_token.delete()
-    return redirect(return_url)
+    
+    return redirect(settings_redirect(request))
 
 @login_required(redirect_field_name='redirect_to')
 def auth(request):
-    request.session['causal_foursquare_oauth_return_url'] = request.GET.get('HTTP_REFERER', None)
+    """Setup oauth details for the return call from foursquare"""
+    request.session['causal_foursquare_oauth_return_url'] = \
+           request.GET.get('HTTP_REFERER', None)
     service = get_model_instance(request.user, MODULE_NAME)
     if not service:
         app = ServiceApp.objects.get(module_name=MODULE_NAME)
@@ -41,7 +51,8 @@ def stats(request, service_id):
     service = get_object_or_404(UserService, pk=service_id)
     template_values = {}
     # get checkins
-    checkins = get_items(request.user, date.today() - timedelta(days=7), service)
+    checkins = get_items(request.user, 
+                         date.today() - timedelta(days=7), service)
     template_values['checkins'] = checkins
     template_values['checkin_centre'] = checkins[-1]
     
