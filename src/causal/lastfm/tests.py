9d9cf101a43ec887c0bdae4f5110675e8ce01a81
Chris Hannam <bassdread@gmail.com>
2011-01-19 12:16:34 +0000

    Added tests for fetching recent tracks to last.fm app

--- src/causal/lastfm/tests.py
+++ src/causal/lastfm/tests.py
@@ -1,23 +1,52 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+"""Tests for last.fm app."""
 
-Replace these with more appropriate tests for your application.
-"""
-
+from causal.lastfm.service import _convert_recent_tracks_json
 from django.test import TestCase
+from django.utils import simplejson
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+try:
+    import wingdbstub
+except ImportError:
+    pass
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class TestLastfmService(TestCase):
+    """Test the module with fixtures."""
 
->>> 1 + 1 == 2
-True
-"""}
-
+    def setUp(self):
+        pass
+        
+    def tearDown(self):
+        pass
+    
+    def test_convert_recent_tracks_json(self):
+        """Test the method for converting a json feed of recent
+        tracks into ServiceItems."""
+        json_file = open(__file__.rpartition('/')[0] + '/test_data/recent_tracks.json', 'r')
+        recent_tracks = json_file.read()
+        json_file.close()
+        
+        service_items = _convert_recent_tracks_json('username', 'lastfm', simplejson.loads(recent_tracks))
+        
+        self.assertEqual(len(service_items), 10)
+        
+    def test_convert_recent_tracks_json_no_recent_tracks(self):
+        """Check it deals with no recent tracks."""
+        
+        json_file = open(__file__.rpartition('/')[0] + '/test_data/no_recent_tracks.json', 'r')
+        recent_tracks = json_file.read()
+        json_file.close()
+        
+        service_items = _convert_recent_tracks_json('username', 'lastfm', simplejson.loads(recent_tracks))
+        
+        self.assertEqual(len(service_items), 0)
+        
+    def test_convert_recent_tracks_json_unknown_user(self):
+        """Check it deals correctly with an unknown username."""
+        
+        json_file = open(__file__.rpartition('/')[0] + '/test_data/unknown_user.json', 'r')
+        recent_tracks = json_file.read()
+        json_file.close()
+        
+        service_items = _convert_recent_tracks_json('username', 'lastfm', simplejson.loads(recent_tracks))
+        
+        self.assertEqual(len(service_items), 0)
