fe730a5ec68640b8fb0f8cdcb3dde3ab6ab64963
Chris Hannam <ch@chrishannam.co.uk>
2011-01-25 18:13:02 +0000

    Convert delicious to use json feed, add first test for changes

--- src/causal/delicious/service.py
+++ src/causal/delicious/service.py
@@ -5,7 +5,7 @@ from django.shortcuts import render_to_response, redirect
 from causal.main.models import AccessToken, ServiceItem
 from dateutil import parser
 from datetime import datetime, timedelta
-import feedparser
+from django.utils import simplejson
 
 DISPLAY_NAME = 'Delicious'
 CUSTOM_FORM = False
@@ -16,20 +16,28 @@ def get_items(user, since, model_instance=None):
     
     at = AccessToken.objects.get(service=serv)
     url = 'http://feeds.delicious.com/v2/rss/%s?count=100' % (at.username,)
-    feed = feedparser.parse(url)
     
-    return _convert_feed(feed, serv, user)
+    user_feed = get_data(
+        serv,
+        url,
+        disable_oauth=True
+        )
     
-def _convert_feed(feed, serv, user):
+    return _convert_feed(serv, user, simplejson.loads(user_feed))
+    
+def _convert_feed(serv, user, json):
+    """Convert the json feed into Service Items"""
+    
     items = []
-    for entry in feed.entries:
+
+    for entry in json:
         item = ServiceItem()
-        item.title = entry.title
-        item.body = entry.link
-        updated = parser.parse(entry.updated)
-        updated = (updated - updated.utcoffset()).replace(tzinfo=None)
-        item.created = updated
-        item.link_back = entry.id
+        item.title = entry['d']
+        item.body = entry['n']
+        item.created = datetime.strptime(entry['dt'], '%Y-%m-%dT%H:%M:%SZ') #'2010-11-23T22:03:29Z'
+        item.link_back = entry['u']
+        item.notes = entry['n']
+        item.tags = entry['t']
         item.service = serv
         item.user = user
         items.append(item)
--- src/causal/delicious/tests.py
+++ src/causal/delicious/tests.py
@@ -1,6 +1,10 @@
-from django.test import TestCase
+"""Test suite for delicious services."""
+
 from causal.delicious.service import _convert_feed
+from django.utils import simplejson
+from django.test import TestCase
 import feedparser
+import os
 
 try:
     import wingdbstub
@@ -11,43 +15,16 @@ class TestDeliciousService(TestCase):
     """Test the module with fixtures."""
 
     def setUp(self):
-        self.rss_feed = """
-    <?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://web.resource.org/cc/">
-  <channel>
-    <title>Delicious/username</title>
-    <link>http://www.delicious.com/username</link>
-    <description>bookmarks posted by username</description>
-    <atom:link rel="self" type="application/rss+xml" href="http://feeds.delicious.com/v2/rss/username?count=100"/>
-    <item>
-      <title>LongURL | API Version 2.0 Documentation</title>
-      <pubDate>Tue, 23 Nov 2010 22:03:29 +0000</pubDate>
-      <guid isPermaLink="false">http://www.delicious.com/url/aacd5c54283976b60786103f1421aab4#username</guid>
-      <link>http://longurl.org/api</link>
-      <dc:creator><![CDATA[username]]></dc:creator>
-      <comments>http://www.delicious.com/url/aacd5c54283976b60786103f1421aab4</comments>
-      <wfw:commentRss>http://feeds.delicious.com/v2/rss/url/aacd5c54283976b60786103f1421aab4</wfw:commentRss>
-      <source url="http://feeds.delicious.com/v2/rss/username">username's bookmarks</source>
-      <category domain="http://www.delicious.com/username/">urls</category>
-      <category domain="http://www.delicious.com/username/">shortners</category>
-    </item>
-  </channel>
-
-</rss>
-"""
-        self.rss_feed_parsed = feedparser.parse(self.rss_feed)
+        self.path = os.path.dirname(os.path.realpath(__file__))
         
     def tearDown(self):
         pass
 
     def test_convert_feed(self):
         """Check we get back a correctly formed list of ServiceItems"""
-        items = _convert_feed(self.rss_feed_parsed, 'causal-delicious', 'username')
-        self.assertEquals(len(items), 1)
+        json_file = open(self.path + '/test_data/user_feed.json', 'r')
+        json_feed = json_file.read()
+        json_file.close()
+        service_items = _convert_feed('username', 'delicious', simplejson.loads(json_feed))
         
-        for item in items:
-            self.assertEquals(item.title, self.rss_feed_parsed.entries[0].title)
-            self.assertEquals(item.body, self.rss_feed_parsed.entries[0].link)
-            self.assertEquals(item.link_back, self.rss_feed_parsed.entries[0].id)
-            self.assertEquals(item.service, 'causal-delicious')
-            self.assertEquals(item.user, 'username')
+        self.assertEqual(len(service_items), 4)
