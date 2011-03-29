fe2e63057dfcfab1a8712068fde5b314b9bb11b9
Wesley Mason <wes@1stvamp.org>
2011-03-29 15:57:11 +0100

    Manual merge of 08d5b0322f631ce2155bacd4e789b9341416e428

--- src/causal/facebook/service.py
+++ src/causal/facebook/service.py
@@ -141,12 +141,13 @@ class ServiceHandler(OAuthServiceHandler):
                                 item.title = photo['name']
                             item.body = photo['picture']
                             item.comments = []
-                            for comment in photo['comments']['data']:
-                                comment_item = ServiceItem()
-                                comment_item.created = datetime.strptime(comment['created_time'].split('+')[0], '%Y-%m-%dT%H:%M:%S')
-                                comment_item.body = comment['message']
-                                comment_item.from_user = comment['from']['name']
-                                item.comments.append(comment_item)
+                            if photo.has_key('comments'):
+                                for comment in photo['comments']['data']:
+                                    comment_item = ServiceItem()
+                                    comment_item.created = datetime.strptime(comment['created_time'].split('+')[0], '%Y-%m-%dT%H:%M:%S')
+                                    comment_item.body = comment['message']
+                                    comment_item.from_user = comment['from']['name']
+                                    item.comments.append(comment_item)
                             item.service = self.service
                             photos.append(item)
 
