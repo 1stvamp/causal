eee983bd0b2000561979f5a6d9d95350f87da5f0
Wesley Mason <wes@1stvamp.org>
2011-03-31 11:47:01 +0100

    Refactored delicious app

--- src/causal/delicious/service.py
+++ src/causal/delicious/service.py
@@ -1,53 +1,55 @@
+"""This file provides the fetching and converting of the
+feed from delicious.com.
+"""
+
+from causal.main.handlers import BaseServiceHandler
 from causal.main.models import ServiceItem
-from causal.main.utils.services import get_model_instance, get_data
+from causal.main.utils.services import get_data
 from causal.main.exceptions import LoggedServiceError
 from django.shortcuts import render_to_response, redirect
-from causal.main.models import AccessToken, ServiceItem
 from dateutil import parser
 from datetime import datetime, timedelta
 from django.utils import simplejson
 
-DISPLAY_NAME = 'Delicious'
-CUSTOM_FORM = False
-OAUTH_FORM = False
+class ServiceHandler(BaseServiceHandler):
+    display_name = 'Delicious'
 
-def get_items(user, since, model_instance=None):
-    serv = model_instance or get_model_instance(user, __name__)
-
-    at = AccessToken.objects.get(service=serv)
-    url = 'http://feeds.delicious.com/v2/json/%s?count=100' % (at.username,)
-
-    user_feed = get_data(
-        serv,
-        url,
-        disable_oauth=True
+    def get_items(self, since):
+        url = 'http://feeds.delicious.com/v2/json/%s?count=100' % (
+            self.service.auth.username,
         )
 
-    return _convert_feed(serv, user, user_feed, since)
+        user_feed = get_data(
+            self.service,
+            url,
+            disable_oauth=True
+        )
 
-def _convert_feed(serv, user, json, since):
-    """Convert the json feed into Service Items limiting on since"""
+        return self._convert_feed(user_feed, since)
 
-    items = []
+    def _convert_feed(self, json, since):
+        """Convert the json feed into Service Items limiting on since.
+        """
 
-    # FIXME add filter on date!
-    for entry in json:
-        # check we have a valid feed
-        if entry != '404 Not Found':
-            item = ServiceItem()
-            created = datetime.strptime(entry['dt'], '%Y-%m-%dT%H:%M:%SZ') #'2010-11-23T22:03:29Z'
-            if created.date() >= since:
-                try:
-                    item.title = entry['d']
-                    item.body = entry['n']
-                    item.created = created
-                    item.link_back = entry['u']
-                    item.notes = entry['n']
-                    item.tags = entry['t']
-                    item.service = serv
-                    item.user = user
-                    items.append(item)
-                except:
-                    pass
+        items = []
 
-    return items
+        # FIXME add filter on date!
+        for entry in json:
+            # Check we have a valid feed
+            if entry != '404 Not Found':
+                item = ServiceItem()
+                created = datetime.strptime(entry['dt'], '%Y-%m-%dT%H:%M:%SZ') #'2010-11-23T22:03:29Z'
+                if created.date() >= since:
+                    try:
+                        item.title = entry['d']
+                        item.body = entry['n']
+                        item.created = created
+                        item.link_back = entry['u']
+                        item.notes = entry['n']
+                        item.tags = entry['t']
+                        item.service = self.service
+                        items.append(item)
+                    except:
+                        pass
+
+        return items
